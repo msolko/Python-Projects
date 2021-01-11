9f80eaef9576b5b7da66cc27f93fee422d335c3f
msolko <67070016+msolko@users.noreply.github.com>
2021-01-11 13:42:34 -0700

    Update Secret Santa.py
    Added complexity by letting people in relationships not get their partner

--- Secret Santa.py
+++ Secret Santa.py
@@ -2,7 +2,7 @@
 #                           Secret Santa Permutations                           #
 #                                                                               #
 # PROGRAMMER:       Maxwell Solko                                               #
-# Date:             11/4/2020                                                   #
+# Date:             1/11/2021                                                   #
 #                                                                               #
 # DESCRIPTION:                                                                  #
 # This program tests how many valid ways of doing a secret santa with N people. #
@@ -19,16 +19,52 @@ import math
 def nlist(n):
     temp = []
     for i in range(n):
-        temp.append(i+1)
+        temp.append(i)
     return temp
 
 # Checks whether anyone is giving gifts to themselves
-def permCheck(lst,legalList):
+def permCheck1(lst,legalList):
     for i in range(len(lst)):
-        if(lst[i]==i+1):
+        if(lst[i]==i):
             return legalList
     legalList+=1
-    return(legalList)
+    return legalList
+
+# Checks whether anyone is giving gifts to themselves or their partner.
+# This assumes that 0 is with 1, 2 is with 3, etc.
+# For odd numbers of people, the last person is single
+def permCheck2(lst, legalList):
+    for i in range(len(lst)):
+        if (i%2==0):
+            if((lst[i]==i) or (lst[i]==i+1)):
+                return legalList
+        else:
+            if((lst[i]==i) or (lst[i]==i-1)):
+                return legalList
+    legalList+=1
+    return legalList
+
+# Checks for gifting themselves or partners, but not everyone has to be partners
+# extra input delineates where partners end and singles start
+# this is the number of relationships, assuming relations are at the start
+# Due to variability, harder to test numerous group numbers without hard coding
+# you could make a list which the for loop uses to find the deliniation each loop
+def permCheck3(lst, legalList, numRelationships):
+    for i in range(len(lst)):
+        if(i<numRelationships*2): #if in partner area, do permcheck2
+            if (i%2==0):
+                if((lst[i]==i) or (lst[i]==i+1)):
+                    return legalList
+            else:
+                if((lst[i]==i) or (lst[i]==i-1)):
+                    return legalList
+
+        else: #If we are in singles, do permcheck1
+            if(lst[i]==i):
+                return legalList
+    legalList+=1
+    return legalList
+                
 
 # Printing Format
 print("=================================================================")
@@ -40,24 +76,24 @@ def goodPrint(n,legalList):
     print("%-13i%-11i%-11i%-15f"%(n,nlst,legalList,perLegal))
 
 
-for i in range(11):
-    leef = 0
+#######################Testing things######################################
+# testing permcheck1 or 2 for up to N people
+for i in range(10):
+    temp = 0
     #Making a list of all permutations of N people
     plst = list(itertools.permutations(nlist(i+1)))
     for k in range(len(plst)):
-        leef = permCheck(plst[k],leef)
-    goodPrint(i+1,leef)
+        temp = permCheck2(plst[k],temp)
+    goodPrint(i+1,temp)
 
-#######################Testing things######################################
-##temp=list(itertools.permutations(nlist(3)))
-##legalList=0
-##print(temp)
-##for i in range(len(temp)):
-##    legalList = permCheck(temp,legalList)
-##    print(temp[i], legalList)
+# testing permcheck3 for 'groupNum' people with 'relationNum' relationships
+groupNum = 8
+relationNum = 2
 
-##temp=list(itertools.permutations(nlist(3)))[3]
-##print(temp)
-##leef =0
-##leef = permCheck(temp,leef)
-##print(leef)
+temp = 0
+#Making a list of all permutations of N people
+plst = list(itertools.permutations(nlist(groupNum)))
+for k in range(len(plst)):
+    temp = permCheck3(plst[k],temp, relationNum)
+goodPrint(groupNum,temp)
+
